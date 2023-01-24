cd26493d9de8ee6809cfd362312bda337686c6e8
David Aparicio Sir <davidapariciosir@gmail.com>
2023-01-24 22:58:35 +0100

    21/01/2023
    Trabajo en Casa

--- conf/confDBPDO.php
+++ conf/confDBPDO.php
@@ -1,35 +1,41 @@
 <?php
-/*
- * Fichero de configuracion que contiene la conexion a la base de datos
- * @author Manuel Martín Alonso
- * @since:  12-01-2023
- * Última modificación: 12-01-2023
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
+
 /*
+ * Fichero de configuracion que contiene la conexion a la base de datos PDO
+ * @author David Aparicio Sir
+ * Fecha Creacion:  14/11/2021
+ * Última modificación: 14/11/2021
+ */
+
 //ENTORNO DESARROLLO CASA
 // IP del servidor y Nombre de la base de datos
-define("DSN", 'mysql:host=192.168.1.204;dbname=DB204DWESLoginLogoff');
-// Usuario con el que se conecta
-define("USER", 'User204DWESLoginLogoff');
-// Contraseña con la que conectarse a la base de datos 
-define("PASSWORD", 'paso');
-*/
-/*
-//ENTORNO DE DESARROLLO
-// IP del servidor y Nombre de la base de datos
-define("DSN", 'mysql:host=192.168.20.19;dbname=DB204DWESLoginLogoff');
+define("DSN", "mysql:dbname=DAW201DBLoginLogoff;host=192.168.3.208");
 // Usuario con el que se conecta 
-define("USER", 'User204DWESLoginLogoff');
+define("USER", "userDAW201LoginLogoff");
 // Contraseña con la que conectarse a la base de datos 
-define("PASSWORD", 'paso');
-*/
+define("PASSWORD", "paso");
 
-//ENTORNO DE EXPLOTACION 1&1
+//ENTORNO DESARROLLO CLASE LOCAL
+/*
+// IP del servidor y Nombre de la base de datos
+define("DSN", "mysql:dbname=DAW201DBLoginLogoff;host=192.168.20.19");
+// Usuario con el que se conecta 
+define("USER", "userDAW201LoginLogoff");
+// Contraseña con la que conectarse a la base de datos 
+define("PASSWORD", "paso");
+*/
+//ENTORNO DESARROLLO 1&1
+/*
 // IP del servidor y Nombre de la base de datos
 define("DSN", "mysql:host=db5010845754.hosting-data.io; dbname=dbs9173955;");
 // Usuario con el que se conecta 
 define("USER", "dbu2895102");
 // Contraseña con la que conectarse a la base de datos 
 define("PASSWORD", "daw2_Sauces");
-
+ */
 ?>
